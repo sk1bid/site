fcd794f7664d42c2ad4bd5f348f943852c973f21
sk1bid <ivanov.artem.bld@gmail.com>
2025-10-11 17:54:23 +0700

    fix error

--- server.js
+++ server.js
@@ -81,8 +81,8 @@ app.get("/api/status", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, "0.0.0.0", () => console.log(`✅ Server running on ${PORT}`));
